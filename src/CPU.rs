cda073ccc2dc83bc42f1d7d73ce47fd4e17b0ec2
Martin <martin.evenbom@gmail.com>
2022-11-26 08:48:34 +0100

    more

--- src/CPU.rs
+++ src/CPU.rs
@@ -9,6 +9,48 @@ pub struct CPU {
 
 
 impl CPU {
+
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>){
+        self.load(program);
+        self.run()
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.program_counter = 0x8000;
+    }
+
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos +1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi)
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let opscode = self.mem_read( self.program_counter);
+            self.program_counter += 1;
+
+            match opscode {
+
+            }
+        }
+    }
     pub fn new() -> Self {
         CPU {
             register_a: 0,
